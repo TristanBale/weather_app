6fec3ea75371dd6d0c1cd8c23463d1f45db335e4
Tristanbale <tristanleecj@gmail.com>
2023-11-30 21:28:56 +0800

    added function to toggle between C and F

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const apiKey = '749bf247d9bb492e9bb192226232511';
 let country = 'singapore';
+let currentMode = 'C';
 const searchForm = document.getElementById('searchForm');
 const searchInput = document.getElementById('searchInput');
+const temperatureToggle = document.querySelector('.temperatureToggle'); 
 const weatherInfoHTML = document.getElementById('weatherInfo');
 
 const locationHTML = document.querySelector('.location');
@@ -28,6 +30,19 @@ const percipChance2 = document.querySelector('.chanceOfRain2');
 const weatherLogo2 = document.querySelector('.weatherLogo2');
 const avgTemp2 = document.querySelector('.avgTemp2');
 
+temperatureToggle.addEventListener('click', () => {
+    if (currentMode == 'C') {
+        currentMode = 'F';
+        temperatureToggle.innerHTML = '<span>°C / <b>°F</b></span>';
+    } else if (currentMode == 'F') {
+        currentMode = 'C';
+        temperatureToggle.innerHTML = '<span><b>°C</b> / °F</span>';
+    }
+    getForecastWeatherData(country).catch(err => {
+        console.log(err.error['message']);
+    });
+});
+
 
 searchForm.addEventListener('submit', async function(event) {
     event.preventDefault();
@@ -36,6 +51,7 @@ searchForm.addEventListener('submit', async function(event) {
     const searchTermProcessed = searchTerm.toLowerCase();
     //console.log(searchTerm);
     console.log(searchTermProcessed);
+    country = searchTermProcessed;
     getForecastWeatherData(searchTermProcessed).catch(err => {
         console.log(err.error['message']);
     });
@@ -56,8 +72,10 @@ async function getForecastWeatherData(country) {
     const feelsLikeF = weatherData.current.feelslike_f;
     const tempC = weatherData.current.temp_c;
     const tempF = weatherData.current.temp_f;
-    const highestTemp = weatherData.forecast.forecastday['0'].day.maxtemp_c;
-    const lowestTemp = weatherData.forecast.forecastday['0'].day.mintemp_c;
+    const highestTempC = weatherData.forecast.forecastday['0'].day.maxtemp_c;
+    const lowestTempC = weatherData.forecast.forecastday['0'].day.mintemp_c;
+    const highestTempF = weatherData.forecast.forecastday['0'].day.maxtemp_f;
+    const lowestTempF = weatherData.forecast.forecastday['0'].day.mintemp_f;
     const chanceOfRain = weatherData.forecast.forecastday['0'].day.daily_chance_of_rain;
 
     const weatherTextInfo = weatherData.current.condition['text'];
@@ -74,32 +92,46 @@ async function getForecastWeatherData(country) {
     const location = weatherData.location.country;
 
    
-
     locationHTML.innerText = location;
     dateAndTime.innerText = localTime;
     weatherLogo.src = weatherInfoIcon;
     weatherDescription.innerText = weatherTextInfo;
-    temperatureActual.innerText = `${tempC}°`;
+    //temperatureActual.innerText = `${tempC}°`;
     //temperatureFeelsLike.innerText = `Feels like ${feelsLikeC}°`;
-    //highLow.innerText = `H: ${highestTemp} L: ${lowestTemp}`;
+    //highLow.innerText = `H: ${highestTempC} L: ${lowestTempC}`;
 
-    weatherTextData.innerText = `Today: ${weatherTextInfo}. Currently feels like ${feelsLikeC}°. The high will be ${highestTemp}°. The low tonight will be ${lowestTemp}°.`;
+    //weatherTextData.innerText = `Today: ${weatherTextInfo}. Currently feels like ${feelsLikeC}°. The high will be ${highestTempC}°. The low tonight will be ${lowestTempC}°.`;
 
     dateAndTime0.innerText = getDaysDigitFromDate(weatherData.forecast.forecastday['0']['date']);
-    percipChance0.innerText = `${chanceOfRain}%`;
+    percipChance0.innerText = `Chance of rain: ${chanceOfRain}%`;
     weatherLogo0.src = weatherData.forecast.forecastday['0'].day.condition['icon'];
-    avgTemp0.innerText = weatherData.forecast.forecastday['0'].day.avgtemp_c;
+    //avgTemp0.innerText = weatherData.forecast.forecastday['0'].day.avgtemp_c;
 
     dateAndTime1.innerText = getDaysDigitFromDate(weatherData.forecast.forecastday['1']['date']);
-    percipChance1.innerText = `${chanceOfRain}%`;
+    percipChance1.innerText = `Chance of rain: ${weatherData.forecast.forecastday['1'].day.daily_chance_of_rain}%`;
     weatherLogo1.src = weatherData.forecast.forecastday['1'].day.condition['icon'];
-    avgTemp1.innerText = weatherData.forecast.forecastday['1'].day.avgtemp_c;
+    //avgTemp1.innerText = weatherData.forecast.forecastday['1'].day.avgtemp_c;
 
     dateAndTime2.innerText = getDaysDigitFromDate(weatherData.forecast.forecastday['2']['date']);
-    percipChance2.innerText = `${chanceOfRain}%`;
+    percipChance2.innerText = `Chance of rain: ${weatherData.forecast.forecastday['2'].day.daily_chance_of_rain}%`;
     weatherLogo2.src = weatherData.forecast.forecastday['2'].day.condition['icon'];
-    avgTemp2.innerText = weatherData.forecast.forecastday['2'].day.avgtemp_c;
+    //avgTemp2.innerText = weatherData.forecast.forecastday['2'].day.avgtemp_c;
 
+    if (currentMode == 'C') {
+        temperatureActual.innerText = `${tempC}°`;
+        weatherTextData.innerText = `Today: ${weatherTextInfo}. Currently feels like ${feelsLikeC}°. The high will be ${highestTempC}°. The low tonight will be ${lowestTempC}°.`;
+        avgTemp0.innerText = weatherData.forecast.forecastday['0'].day.avgtemp_c;
+        avgTemp1.innerText = weatherData.forecast.forecastday['1'].day.avgtemp_c;
+        avgTemp2.innerText = weatherData.forecast.forecastday['2'].day.avgtemp_c;
+    }
+
+    if (currentMode == 'F') {
+        temperatureActual.innerText = `${tempF}°`;
+        weatherTextData.innerText = `Today: ${weatherTextInfo}. Currently feels like ${feelsLikeF}°. The high will be ${highestTempF}°. The low tonight will be ${lowestTempF}°.`;
+        avgTemp0.innerText = weatherData.forecast.forecastday['0'].day.avgtemp_f;
+        avgTemp1.innerText = weatherData.forecast.forecastday['1'].day.avgtemp_f;
+        avgTemp2.innerText = weatherData.forecast.forecastday['2'].day.avgtemp_f;
+    }
 
 }
 
